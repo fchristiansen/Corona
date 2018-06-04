c668e7c90d2444015eca5aaab7274b42e1df72d2
fchristiansen <felipe@seo2.cl>
2018-06-04 17:29:55 -0400

    Initial commit

---
+++ assets/js/corona.js
@@ -0,0 +1,124 @@
+//modal altura  máxima
+
+function setModalMaxHeight(element) {
+  this.$element     = $(element);
+  this.$content     = this.$element.find('.modal-content');
+  var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
+  var dialogMargin  = $(window).width() < 768 ? 20 : 60;
+  var contentHeight = $(window).height() - (dialogMargin + borderWidth);
+  var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
+  var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
+  var maxHeight     = contentHeight - (headerHeight + footerHeight);
+
+  this.$content.css({
+      'overflow': 'hidden'
+  });
+
+  this.$element
+    .find('.modal-body').css({
+      'max-height': maxHeight,
+      'overflow-y': 'auto'
+  });
+}
+
+$('.modal').on('show.bs.modal', function() {
+  $(this).show();
+  setModalMaxHeight(this);
+});
+
+$(window).resize(function() {
+  if ($('.modal.in').length != 0) {
+    setModalMaxHeight($('.modal.in'));
+  }
+});
+
+//
+
+//menu
+// Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    $('#main_nav').toggleClass('show-nav');
+
+
+  });
+/*--*/
+
+  // ===== smooth scroll to section====
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+/*--*/
+
+// ===== Scroll to Top ====
+  $('#to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0               // Scroll to top of body
+      }, 1500);
+  });
+/*--*/
+
+//cierra navegacion al click fura del contenedor
+	var anchoVentana = $(window).width();
+	if(anchoVentana >= 991){
+
+		$(window).click(function() {
+			$('#main_nav').removeClass('show-nav');
+			$('button.hamburger').removeClass('is-active');
+		});
+		$('#main_nav, button.hamburger').click(function(event){
+		    event.stopPropagation();
+		});
+	}
+	if(anchoVentana <= 991){
+		//colapsa menu en mobile
+   		 $('.navbar-inner ul li a').on('click', function(){
+            $('button.hamburger').click();
+        });
+
+	}
+
+
+
+/*--*/
+
+
+$(window).on('scroll', function(event){
+   var scrollPos = $(document).scrollTop();
+   $(".nav li a").each(function () {
+     var currLink = $(this);
+     var refElement = $(currLink.attr("href"));
+
+     if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+       currLink.parent().addClass("active").siblings().removeClass("active");
+       return;
+     }
+     else{
+       currLink.parent().removeClass("active");
+     }
+   })
+ })
+
+/*--*/
+
+  // colapsa menu al click en un enlace
+        // $('.navbar-inner ul li a').on('click', function(){
+        //     $('button.hamburger').click()
+        // });
+
+
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php include('header.php'); ?>
+  <body>
+  	<header id="home">
+
+	<button class="hamburger hamburger--spin" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	  <span class="hamburger-box">
+	    <span class="hamburger-inner"></span>
+	  </span>
+	</button>
+    <nav id="main_nav" class="navbar navbar-default navbar-fixed-top center">
+      <div class="container">
+        <div id="navbar" class="navbar-collapse collapse">
+        	<div class="navbar-inner">
+        		<ul class="nav navbar-nav">
+        			<li><a href="#home">Home</a></li>
+        			<li><a href="#productos">productos</a></li>
+        			<li><a href="#accesorios">accesorios</a></li>
+        			<li><a href="#drinks">corona drinks</a></li>
+        			<li><a href="#parleyvxo">corona x parley</a></li>
+        			<li><a href="#contacto">contacto</a></li>
+        		</ul>
+        	</div>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+		<!-- <nav id="nav_mobile" class="navbar navbar-default navbar-fixed-top center">
+		  nav_mobile
+		</nav> -->
+	  	<section id="video_top" class="section">
+	  			<img class="img-responsive" src="https://api.fnkr.net/testimg/1920x800/00CED1/FFF/?text=img+placeholder">
+	  	</section> <!-- video -->
+  	</header>
+  	<section id="productos" class="section">
+<!--   		tapa intro + slider interno con 4 productos
+  		slider interno
+  			imagen de fondo grande
+  			caja detalle producto
+  			vista rapida o mas grande + zoom
+  			boton comprar
+  		boton volver a tapa inicial -->
+  		<img class="img-responsive" src="https://api.fnkr.net/testimg/1920x800/000/FFF/?text=img+placeholder">
+
+  	</section><!-- productos -->
+  	<section id="accesorios" class="section">
+	<!-- 	tapa intro con boton ver accesorios
+		se abre un interno con 5 productos (serán mas?) ¿?
+		caja producto
+			imagen producto
+			nombre y detalles producto
+			boton comprar amarillo
+		boton volver
+		se expande fuera de la caja ¿? -->
+		<img class="img-responsive" src="https://api.fnkr.net/testimg/1920x800/f5f5f5/FFF/?text=img+placeholder">
+  	</section><!-- accesorios -->
+  	<section id="drinks" class="section">
+	<!-- 	tapa intro con boton a slider interno
+		logo corona drinks
+		imagen drink + receta drink
+		btn ver video tira modal con el video
+		boton volver blanco -->
+		<img class="img-responsive" src="https://api.fnkr.net/testimg/1920x800/e4e4e4/FFF/?text=img+placeholder">
+  	</section><!-- drinks -->
+  	<section id="parleyvxo" class="section">
+		<section id="parley">
+	<!-- 		btn entrar
+			interior
+				grafica air
+				btn volver azul -->
+		</section> <!-- parley -->
+		<section id="vxo">
+	<!-- 		btn entrar
+			interior
+				embajadores
+				slider tips
+				video
+				mapa
+				btn vovler azul -->
+		</section> <!-- vxo -->
+  	</section><!-- parleyvxo -->
+  	<section id="contacto" class="section">
+  		<img class="img-responsive" src="https://api.fnkr.net/testimg/1920x800/273136/FFF/?text=img+placeholder">
+
+
+  	</section><!-- contacto -->
+
+
+<?php include('footer.php'); ?>
